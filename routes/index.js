08b1877361d5605e77744bbf5d26777b7a9de8c9
seanmcquaid <sean.mcquaid2@verizon.net>
2019-01-30 23:53:33 -0500

    tried to do api calls but failed with post request

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
+const config = require("../config");
+
 const request = require("request");
+
+const apiBaseUrl = "https://trackapi.nutritionix.com/";
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -10,6 +14,12 @@ router.get('/', function(req, res, next) {
 
 router.post("/search/food", (req,res,next)=>{
   const foodName = req.body.foodName;
+  // res.json(foodName);
+  const searchUrl = `${apiBaseUrl}/v2/natural/nutrients/`;
+  const header = {"content-Type": "application/json", "x-app-id" :`${config.apiAppId}`, "x-app-key":`${config.apiKey}`, "x-remote-user-id":`${config.activeUser}`}
+  request.post(searchUrl, {headers: header},(error,res,body)=>{
+  console.log(body)
+  });
 });
 
 module.exports = router;
